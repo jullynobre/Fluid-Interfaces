931886a3987431ffa4a3f28282b409034ec4e250
jullynobre <jullynobre1@gmail.com>
2018-07-25 11:30:32 -0300

    pastel colors and rename project

--- draw-tool/draw-tool/ViewController.swift
+++ draw-tool/draw-tool/ViewController.swift
@@ -31,9 +31,6 @@ class ViewController: UIViewController {
         
         animator = UIDynamicAnimator(referenceView: view)
         
-        //gravit = UIGravityBehavior(items: [square])
-        //animator.addBehavior(gravit)
-        
         collision = UICollisionBehavior(items: [square])
         collision.translatesReferenceBoundsIntoBoundary = true
         animator.addBehavior(collision)
@@ -48,43 +45,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    
     @IBAction func didDrag(_ sender: Any) {
         let gesture: UIPanGestureRecognizer = sender as! UIPanGestureRecognizer
-        switch gesture.state {
-            case .changed:
-                let translation = gesture.translation(in: view)
-                
-                if let view = gesture.view {
-                    let centerX = min(max(translation.x + view.center.x, view.frame.width / 2), self.view.bounds.size.width - view.frame.width / 2)
-                    let centerY = min(max(translation.y + view.center.y, view.frame.height / 2), self.view.bounds.size.height - view.frame.height / 2)
-                    
-                    view.center = CGPoint(x: centerX, y: centerY)
-                }
-                
-                gesture.setTranslation(CGPoint.zero, in: view)
+        
+        if gesture.state == .changed {
+            let translation = gesture.translation(in: view)
             
-            case .ended:
+            if let view = gesture.view {
+                let centerX = min(max(translation.x + view.center.x, view.frame.width / 2), self.view.bounds.size.width - view.frame.width / 2)
+                let centerY = min(max(translation.y + view.center.y, view.frame.height / 2), self.view.bounds.size.height - view.frame.height / 2)
+                
+                view.center = CGPoint(x: centerX, y: centerY)
+            }
             
-                let velocity = gesture.velocity(in: view)
-                let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
-                print(velocity)
-                let slideMultiplier = magnitude / 900
-                let sliderFactor = 0.1 * slideMultiplier
-                
-                var finalPoint = CGPoint(x: gesture.view!.center.x + (velocity.x * sliderFactor),
-                                         y: gesture.view!.center.y + (velocity.y * sliderFactor))
-                
-                finalPoint.x = min(max(finalPoint.x,square.frame.width / 2), self.view.bounds.size.width - square.frame.width / 2)
-                finalPoint.y = min(max(finalPoint.y, square.frame.height / 2), self.view.bounds.size.height - square.frame.height / 2)
-                
-                UIView.animate(withDuration: Double(sliderFactor * 2), delay: 0, options: [.curveEaseOut, .allowUserInteraction], animations: {
-                    gesture.view!.center = finalPoint
-                }, completion: nil)
+            gesture.setTranslation(CGPoint.zero, in: view)
+        } else if gesture.state == .ended {
+            let velocity = gesture.velocity(in: view)
+            let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
+            print(velocity)
+            let slideMultiplier = magnitude / 900
+            let sliderFactor = 0.1 * slideMultiplier
             
-            default:
-                return
+            var finalPoint = CGPoint(x: gesture.view!.center.x + (velocity.x * sliderFactor),
+                                     y: gesture.view!.center.y + (velocity.y * sliderFactor))
+            
+            finalPoint.x = min(max(finalPoint.x,square.frame.width / 2), self.view.bounds.size.width - square.frame.width / 2)
+            finalPoint.y = min(max(finalPoint.y, square.frame.height / 2), self.view.bounds.size.height - square.frame.height / 2)
+            
+            UIView.animate(withDuration: Double(sliderFactor * 2), delay: 0, options: [.curveEaseOut, .allowUserInteraction], animations: {
+                gesture.view!.center = finalPoint },
+                           completion: nil)
         }
     }
     
@@ -112,21 +102,22 @@ class ViewController: UIViewController {
     @IBAction func didPinch(_ sender: Any) {
         let gesture: UIPinchGestureRecognizer = sender as! UIPinchGestureRecognizer
         
-        gesture.view?.transform = (gesture.view?.transform.scaledBy(x: gesture.scale, y: gesture.scale))!
+        let maxScale = self.view.frame.width / (gesture.view?.frame.width)!
+        
+        if gesture.scale < maxScale {
+            gesture.view?.transform = (gesture.view?.transform.scaledBy(x: gesture.scale, y: gesture.scale))!
+        }
+        
         gesture.scale = 1.0
     }
     
     func randomColor() -> UIColor {
-        let red = CGFloat(drand48())
-        let green = CGFloat(drand48())
-        let blue = CGFloat(drand48())
-        
-        return UIColor(red: red, green: green, blue: blue, alpha: 1)
+        return UIColor.init(hue: CGFloat(drand48()), saturation: 0.3, brightness: 1, alpha: 1)
     }
     
 }
+
 extension ViewController: UIGestureRecognizerDelegate{
-    
     func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
         return true
     }
